c99048012563d55a31c36b497d1f14f75b2714b9
pollux2021 <97222562+pollux2021@users.noreply.github.com>
2022-01-18 09:52:10 +0800

    修改获取visitorId, 在localhost与http不一致

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-import FingerprintJS from '@fingerprintjs/fingerprintjs'
+import FingerprintJS, { murmurX64Hash128 } from "@fingerprintjs/fingerprintjs"
 
-async function getResultSync  () {
-  const fpPromise = FingerprintJS.load()
-  const fp = await fpPromise
-  const result = await fp.get()
-  return result
+function componentsToCanonicalString(components: any) {
+	let result = ""
+
+	console.log(components)
+	for (const componentKey of Object.keys(components).sort()) {
+		const component = components[componentKey]
+		const value = component.error ? "error" : JSON.stringify(component.value)
+		result += `${result ? "|" : ""}${componentKey.replace(
+			/([:|\\])/g,
+			"\\$1"
+		)}:${value}`
+	}
+	return result
 }
 
-async function getVisitorIdSync  (): Promise<String>  {
-  const fpPromise = FingerprintJS.load()
-  const fp = await fpPromise
-  const result = await fp.get()
-  return result.visitorId
+async function getResultSync() {
+	const fpPromise = FingerprintJS.load()
+	const fp = await fpPromise
+	const result = await fp.get()
+	return result
 }
 
-export {getVisitorIdSync, getResultSync}
+async function getVisitorIdSync(): Promise<String> {
+	const fpPromise = FingerprintJS.load()
+	const fp = await fpPromise
+	const result = await fp.get()
+	const components = result.components
+	components.cookiesEnabled.value = true
+	return murmurX64Hash128(componentsToCanonicalString(components))
+}
 
-
+export { getVisitorIdSync, getResultSync }
